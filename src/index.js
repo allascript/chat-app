37cba0ba571d2d778f109ad11b25f02ff6f8dbbd
Alla Udovenko <ab.udovenko@gmail.com>
2020-05-25 12:14:35 +0200

    Setting up Firebase in a React app, authentification users and conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,23 +6,47 @@ import App from './App';
 import Login from './components/Login';
 import Register from './components/Register';
 
+import firebase, {auth, provider} from './firebase.js';
+
 
 
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 class AppRouter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {user: null}
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if(user){
+        this.setState({user});
+      }
+    })
+  }
+  logOutUser = () => {
+    firebase.auth().signOut()
+    .then(window.location = "/");
+  }
+
   render(){
     return (
 <Router>
-<div name className="app">
+  <div name className="app">
   <nav className="main-nav">
-    <Link to="/">Home</Link>
+    {!this.state.user &&
+  <div>
     <Link to="/login">Login</Link>
     <Link to="/register">Register</Link>
+    </div>
+    }
+    {this.state.user &&
+    <a href="#!" onClick={this.logOutUser}>Logout</a>
+  }
   </nav>
   <Switch>
-  <Route path="/"exact component={App} />
+  <Route path="/"exact render={() => <App user={this.state.user} />}/>
   <Route path="/login"exact component={Login} />
   <Route path="/register"exact component={Register} />
   </Switch>
